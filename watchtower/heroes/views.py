ac04de06cb7e15088a6cc0891209e4380a029a0a
TBuscemi <thomasbuscemi124@gmail.com>
2021-07-31 11:54:47 -0400

    added the heros to the data base . fixed the myapp to heroes so it finds the templete.

--- watchtower/heroes/views.py
+++ watchtower/heroes/views.py
@@ -1,8 +1,15 @@
 # from typing_extensions import runtime
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Heroes
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HELLO WORLD!")
+    all_heroes = Heroes.objects.all()
+    context = {
+        'all_movies': all_heroes
+    }
+    return render(request,'heroes/index.html')
+
+
     
