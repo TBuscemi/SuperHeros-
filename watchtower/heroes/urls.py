35f61033d01f4edec3e80acc5836305293892dbb
TBuscemi <thomasbuscemi124@gmail.com>
2021-08-01 13:46:04 -0400

    got the creat html pager working

--- watchtower/heroes/urls.py
+++ watchtower/heroes/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "heroes"
 urlpatterns = [
-    path('', views.index, name ='index')
+    path('', views.index, name ='index'),
+    path('<int:heroes_id/', views.detail, name = 'detail'),
+    path('new/', views.create, name='create_hero')
 ]
--- watchtower/heroes/views.py
+++ watchtower/heroes/views.py
@@ -1,15 +1,39 @@
 # from typing_extensions import runtime
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from.models import Heroes
+from django.urls import reverse
 # Create your views here.
+
 
 def index(request):
     all_heroes = Heroes.objects.all()
     context = {
-        'all_movies': all_heroes
+        'all_heroes': all_heroes
     }
-    return render(request,'heroes/index.html')
+    return render(request,'heroes/index.html',context)
+
+def detail (request,heroes_id):
+    heroes_id = Heroes.objects.get(pk=heroes_id),
+    return render(request, 'heroes/index.html')
+
+def create(request):
+    if request.method == 'POST':
+        super_hero_name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_power = request.POST.get('primary_power')
+        secondary_power = request.POST.get('secondary_power')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Heroes(super_hero_name = super_hero_name, 
+        alter_ego = alter_ego, 
+        primary_power = primary_power, 
+        secondary_power = secondary_power, 
+        catchphrase = catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('heroes:index'))
+    else:
+        return render(request, 'heroes/create.html')
 
 
     
+ 
